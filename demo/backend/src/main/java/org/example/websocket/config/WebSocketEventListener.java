e20d01d1874a75ca4962432aa647568ed41804e9
Hsinlung <1959925904@qq.com>
2025-12-07 20:31:56 +0800

    fixed

--- demo/backend/src/main/java/org/example/websocket/config/WebSocketEventListener.java
+++ demo/backend/src/main/java/org/example/websocket/config/WebSocketEventListener.java
@@ -1,16 +1,12 @@
 package org.example.websocket.config;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.example.service.game.GameService;
 import org.springframework.context.event.EventListener;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 @Component
-@RequiredArgsConstructor
 @Slf4j
 public class WebSocketEventListener {
 
